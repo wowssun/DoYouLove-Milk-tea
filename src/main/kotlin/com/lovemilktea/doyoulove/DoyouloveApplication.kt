0f3938bdcd06e63ee33619f6f22777f04a894e85
wowssun <wow.radiant.ssun@gmail.com>
2024-10-04 22:15:21 +0900

    :tada: [미션2] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/lovemilktea/doyoulove/DoyouloveApplication.kt
@@ -0,0 +1,11 @@
+package com.lovemilktea.doyoulove
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DoyouloveApplication
+
+fun main(args: Array<String>) {
+	runApplication<DoyouloveApplication>(*args)
+}
---
+++ src/test/kotlin/com/lovemilktea/doyoulove/DoyouloveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lovemilktea.doyoulove
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DoyouloveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
